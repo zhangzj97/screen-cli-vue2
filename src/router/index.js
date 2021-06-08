a41c526a5e9a75c4808c85dcf0253e9004a14423
zhangzejie <987883389@qq.com>
2021-06-08 17:38:08 +0800

    feat: Add 简易路由 说明

--- src/router/index.js
+++ src/router/index.js
@@ -5,14 +5,27 @@ import { AutoImportRouters } from '@/vender/z-tools/AutoImport'
 // TODO 键盘 切换 路由
 // import KeyboardRouter     from '@/vender/ztools/KeyboardRouter'
 
+// *** [Screen-cli] [2-1] 引入 有特殊需求的路由组件 开始
+// ***
+// *** [Screen-cli] [2-1] 引入 有特殊需求的路由组件 结束
+
 const routers = [
+  // ...new KeyboardRouter(),
+  // *** [Screen-cli] [1-1] 自动引入 常规路由组件 开始
   ...new AutoImportRouters(require.context('@/views/Main', true, /\.vue$/)).getModules(),
-];
+  ...new AutoImportRouters(require.context('@/views/Main', true, /\.vue$/), { prefix: '/Demo' }).getModules(),
+  // *** [Screen-cli] [1-1] 自动引入 常规路由组件 结束
+]
 
 Vue.use(VueRouter)
 
 const routes = [
+  // 自动引入模块
   ...routers
+
+  // *** [Screen-cli] [2-2] 配置 有特殊需求的路由组件 开始
+  // ***
+  // *** [Screen-cli] [2-2] 配置 有特殊需求的路由组件 结束
 ]
 
 const router = new VueRouter({ mode: 'history', base: process.env.BASE_URL, routes })
