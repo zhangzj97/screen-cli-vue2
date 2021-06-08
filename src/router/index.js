0388e77065dbfae3e2596d93b5c5e4c8095da6e5
zhangzejie <987883389@qq.com>
2021-06-08 16:13:24 +0800

    feat: Add 自动引入 router 模块

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,18 @@
 import Vue        from 'vue'
 import VueRouter  from 'vue-router'
 // TODO 自动引入模块功能
-// import AutoImport from '@/vender/z-tools/AutoImport011'
+import { AutoImportRouters } from '@/vender/z-tools/AutoImport'
+// TODO 键盘 切换 路由
+// import KeyboardRouter     from '@/vender/ztools/KeyboardRouter'
 
-// const routeMain = new AutoImport('@/views/Main', /\.vue$/, false).vue2Router();
+const routers = [
+  ...new AutoImportRouters(require.context('@/views/Main', true, /\.vue$/)).getModules(),
+];
 
 Vue.use(VueRouter)
 
 const routes = [
-  // ...routeMain
+  ...routers
 ]
 
 const router = new VueRouter({ mode: 'history', base: process.env.BASE_URL, routes })
---
+++ src/vender/z-tools/AutoImport.js
@@ -0,0 +1,48 @@
+export class AutoImport {
+    constructor (webpackContext, option) {
+        // ? 关于 Critical dependency 寻求解决方法
+        // Webpack require.context
+        this._webpackContext  = webpackContext;        
+        // 模块 相应的路径
+        this._filePath        = webpackContext.keys();  
+        // 模块 相应的格式
+        this._modules         = {};                    
+        
+        // 相应的配置
+        this._option          = option;      
+    }
+
+    getModules () { throw new Error('[z-tools] 不能直接使用') }
+    getOption  () { throw new Error('[z-tools] 不能直接使用') }
+}
+
+export class AutoImportRouters extends AutoImport {
+    constructor (webpackContext, option = {}) {
+        super(webpackContext, option)
+    }
+
+    getModules (ext = /\.vue$/) {
+        let modules = []
+        modules = this._filePath.map(item => ({
+            path     : `${item.replace(ext, '').slice(1)}`,
+            name     : `${item.replace(ext, '').slice(2)}`,
+            component: this._webpackContext(item).default,
+        }));
+        
+        this._modules = modules;
+        return this._modules;
+    }
+}
+
+export class AutoImportStore extends AutoImport {
+    constructor (webpackContext, option = {}) {
+        super(webpackContext, option)
+    }
+
+    getModules (ext = /\.js$/) {
+        const modules = {};
+        this._filePath.forEach(item => modules[item.replace(ext, '').slice(2)] = this._webpackContext(item).default);
+        this._modules = modules;
+        return this._modules;
+    }
+}
